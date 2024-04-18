cb73782a2d350f4a294028c980a9a7f8dfd8bdfd
pbeg2 <166719334+pbeg2@users.noreply.github.com>
2024-04-18 18:02:30 +0200

    Update jason.rb

--- jason.rb
+++ jason.rb
@@ -1,21 +1,18 @@
-require 'json'
+#require 'json'
 
 # Leer el contenido del archivo JSON
-json_content = File.read('horario_clases.json')
-
+#json_content = File.read('horario_clases.json')
 
 # Parsear el JSON
-datos = JSON.parse(json_content)
+#datos = JSON.parse(json_content)
 
 # Acceder a la lista de clases y mostrar información sobre cada una
-datos['horario'].each do |clase|
-  puts "Clase: #{clase['clase']}"
-  puts "Hora: #{clase['hora']}"
-  puts "Aula: #{clase['aula']}"
-  puts "-" * 20
-end
-
-
+#datos['horario'].each do |clase|
+# puts "Clase: #{clase['clase']}"
+# puts "Hora: #{clase['hora']}"
+# puts "Aula: #{clase['aula']}"
+# puts "-" * 20
+# end
 
 require 'net/http'
 require 'json'
