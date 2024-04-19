600e2a9dfc7877ed5f6cf5230cc3c5cfcdacd597
pbeg2 <166719334+pbeg2@users.noreply.github.com>
2024-04-19 18:11:24 +0200

    Create horarios.rb

---
+++ horarios.rb
@@ -0,0 +1,34 @@
+{
+  "Timetables": [
+    {
+      "day": "Lunes",
+      "hour": "8:00 - 9:30",
+      "subject": "Matemáticas",
+      "room": "A101"
+    },
+    {
+      "day": "Lunes",
+      "hour": "9:45 - 11:15",
+      "subject": "Ciencias",
+      "room": "B205"
+    },
+    {
+      "day": "Lunes",
+      "hour": "11:30 - 13:00",
+      "subject": "Historia",
+      "room": "C309"
+    },
+    {
+      "day": "Martes",
+      "hour": "14:00 - 15:30",
+      "subject": "Inglés",
+      "room": "A101"
+    },
+    {
+      "day": "Martes",
+      "hour": "15:45 - 17:15",
+      "subject": "Arte",
+      "room": "B205"
+    }
+  ]
+}
