ee6227afb531d299ce6bba6927c0517adc8cd865
pbeg2 <166719334+pbeg2@users.noreply.github.com>
2024-04-20 23:00:44 +0200

    Update and rename css.rb to Css.rb

---
+++ Css.rb
@@ -0,0 +1,19 @@
+require 'gtk3'
+def iniciar_ventana(titulo, headers)
+# Crear la ventana para mostrar los datos
+  ventana = Gtk::Window.new
+  ventana.set_title(titulo)
+  ventana.set_default_size(400, 300)
+
+# Crear un contenedor de tipo Grid
+  grid = Gtk::Grid.new
+  grid.set_row_spacing(5)
+  grid.set_column_spacing(5)
+  ventana.add(grid)
+
+# Encabezados
+  headers.each_with_index do |encabezado, index|
+    header_label = Gtk::Label.new(encabezado)
+    grid.attach(header_label, index, 0, 1, 1)
+  end
+end
