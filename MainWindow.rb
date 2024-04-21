4911834fdf235b8139ed99b75f22785349201330
pbeg2 <166719334+pbeg2@users.noreply.github.com>
2024-04-21 20:26:44 +0200

    Update MainWindow.rb

--- MainWindow.rb
+++ MainWindow.rb
@@ -1,28 +1,40 @@
+require "gtk3"
 require "thread"
 require_relative 'LCDController'
 require_relative 'Rfid'
 
 class MainWindow < Gtk::Window
     def initialize(lcd_controller)
-        @lcd_controller = lcd_controller # Crear la instancia de LCDController        
+        @lcd_controller = lcd_controller # Crear la instancia de LCDController
+        
         @window = Gtk::Window.new("course_manager.rb")
-        @window.set_default_size(500, 200) # Configurar el tamaño de la ventana   
+        @window.set_default_size(500, 200) # Configurar el tamaño de la ventana
+        
+        
+        @thread = nil  # Inicializar el hilo como nulo al principio
         # Conectar la señal "destroy" para cerrar la aplicación cuando se cierra la ventana
         @window.signal_connect("destroy") do
             Gtk.main_quit
             @thread.kill if @thread #Detiene la ejecución del thread
+            Gtk.main_quit
         end 
+
         ventana_inicio # Crear el contenido de la ventana_inicio
+        
+
     end   
 
     def ventana_inicio
+        @window.children.each{|widget| @window.remove(widget)}
         # Crear un marco para enmarcar el mensaje
         @frame = Gtk::Frame.new
         @frame.set_border_width(10)
-        @frame.override_background_color(:normal, Gdk::RGBA.new(0, 0, 1, 1)) # Color azul        
+        @frame.override_background_color(:normal, Gdk::RGBA.new(0, 0, 1, 1)) # Color azul
+        
         # Crear un contenedor Gtk::Box dentro del marco para organizar verticalmente
         box = Gtk::Box.new(:vertical, 5)
         @frame.add(box)
+
         # Mensaje antes de la autenticación
         @label = Gtk::Label.new("Please, login with your university card")
         @label.override_color(:normal, Gdk::RGBA.new(1, 1, 1, 1)) # Color blanco
@@ -53,11 +65,9 @@ class MainWindow < Gtk::Window
     end
 
     def autenticacion(uid)
-        uri =URI("http://127.0.0.1:9000/students?student_id=#{uid}")
-	response = NET::HTTP.get(uri)
-        if response.code == '200'
-            datos=JSON.parse(response.body)
-	    puts "#{datos.name}"
+        
+        if @uid == "60B69521"
+            @nombre = "Pepito"
             ventana_query 
         else 
             @lcd_controller.escribir_en_lcd("Authentication error please try again.")
@@ -72,97 +82,29 @@ class MainWindow < Gtk::Window
     end
 
     def ventana_query
-        #empezar timeout
-	timeout
+        # Empezar timeout
+    
         @frame.destroy
         # Mostrar el mensaje en la LCD
         @lcd_controller.escribir_en_lcd_centrado("Welcome #{@nombre}")
-
+    
         @nombre = Gtk::Label.new("Welcome #{@nombre}")
-        
     
         # Crear el campo de entrada para el query
         @query_entry = Gtk::Entry.new
-        @query_entry.set_placeholder_text("Ingrese query (timetable, tasks, marks)")
+        @query_entry.set_placeholder_text("Ingrese query (timetables, tasks, marks)")
     
         # Crear el botón de logout
         @button = Gtk::Button.new(label: 'logout')
         @button.set_size_request(50, 50)
-        @button.signal_connect('clicked') {ventana_inicio}
+        @button.signal_connect('clicked') { ventana_inicio }
     
         # Manejar el evento 'activate' (presionar Enter)
         @query_entry.signal_connect("activate") do
             query = @query_entry.text.strip.downcase
+            tabla_informacion(query) # Llamar a tabla_informacion con el query ingresado
         end
-
-        @table = Gtk::Table.new(2,2,false) 
-		@table.set_column_spacing(300) 
-		@table.set_row_spacings(5) 
-
-        @table.attach(@nombre, 0,  1,  0,  1, Gtk::AttachOptions::SHRINK, Gtk::AttachOptions::SHRINK, 10 , 0)
-		@table.attach(@button, 1,  2,  0,  1, Gtk::AttachOptions::SHRINK, Gtk::AttachOptions::SHRINK, 30, 10)
-		@table.attach(@query_entry, 0,  2,  1,  2, Gtk::AttachOptions::FILL, Gtk::AttachOptions::EXPAND, 10, 0)
-		
-		@window.add(@table)
-        @window.show_all
-    end
-	
-#    def timeout
-#	GLib::Timeout.add_seconds(20) do
-#          puts "Se han superado los 20 segundos."
-#          ventana_inicio # Llamar al método ventana_inicio si se supera el tiempo límite
-#          false 	          # Importante: Devolver false para que el temporizador no se repita
-#	end
-#    end
-
-    def tabla_informacion(query)
-        case query      
-        when "timetables"
-            mostrar_datos_json('http://ejemplo.com/horario', 'Timetables', ['Di­a', 'Hora', 'Materia', 'Aula'])
-        when "tasks"
-            mostrar_datos_json('http://ejemplo.com/tasks', 'Tasks', ['Fecha', 'Materia', 'Nombre'])
-        when "marks"
-            mostrar_datos_json('http://ejemplo.com/marks', 'Marks', ['Asignatura', 'Nombre', 'Nota'])
-        else
-#	    timeout
-            puts "Consulta no valida: #{query}"
-        end
-      
-        query_entry.text = "" # Limpiar el campo de entrada despues de la consulta
-        #reiniciar timeout
-#	timeout
-    end
-end
-
-lcd_controller = LCDController.new # Crear una instancia de LCDController
-
-# Ejecutar la aplicación
-MainWindow.new(lcd_controller)
-Gtk.main
-
-    def ventana_query
-        #empezar timeout
-#	timeout
-        @frame.destroy
-        # Mostrar el mensaje en la LCD
-        @lcd_controller.escribir_en_lcd_centrado("Welcome #{@nombre}")
-
-        @nombre = Gtk::Label.new("Welcome #{@nombre}")
-        
-        # Crear el campo de entrada para el query
-        @query_entry = Gtk::Entry.new
-        @query_entry.set_placeholder_text("Ingrese query (timetable, tasks, marks)")
     
-        # Crear el botón de logout
-        @button = Gtk::Button.new(label: 'logout')
-        @button.set_size_request(50, 50)
-        @button.signal_connect('clicked') {ventana_inicio}
-    
-        # Manejar el evento 'activate' (presionar Enter)
-        @query_entry.signal_connect("activate") do
-            query = @query_entry.text.strip.downcase
-        end
-
         @table = Gtk::Table.new(2,2,false) 
 		@table.set_column_spacing(300) 
 		@table.set_row_spacings(5) 
@@ -175,23 +117,6 @@ Gtk.main
         @window.show_all
     end
     
-    def tabla_informacion(query)
-        case query      
-        when "timetables"
-            mostrar_datos_json('http://ejemplo.com/horario', 'Timetables', ['Di­a', 'Hora', 'Materia', 'Aula'])
-        when "tasks"
-            mostrar_datos_json('http://ejemplo.com/tasks', 'Tasks', ['Fecha', 'Materia', 'Nombre'])
-        when "marks"
-            mostrar_datos_json('http://ejemplo.com/marks', 'Marks', ['Asignatura', 'Nombre', 'Nota'])
-        else
-	    timeout
-            puts "Consulta no valida: #{query}"
-        end
-     
-        query_entry.text = "" # Limpiar el campo de entrada despues de la consulta
-        #reiniciar timeout
-#	timeout
-    end
 end
 
 lcd_controller = LCDController.new # Crear una instancia de LCDController
