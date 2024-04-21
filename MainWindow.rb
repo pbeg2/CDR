bbe950a5b008d3a57e4959951a2db3ecaaf9b136
pbeg2 <166719334+pbeg2@users.noreply.github.com>
2024-04-21 16:47:15 +0200

    Update MainWindow.rb

--- MainWindow.rb
+++ MainWindow.rb
@@ -1,4 +1,3 @@
-require "gtk3"
 require "thread"
 require_relative 'LCDController'
 require_relative 'Rfid'
@@ -63,87 +62,7 @@ class MainWindow < Gtk::Window
     end
 
     def autenticacion(uid)
-        
-        if @uid == "60B69521"
-            @nombre = "Pepito"
-            ventana_query 
-        else 
-            @lcd_controller.escribir_en_lcd("Authentication error please try again.")
-            @label.set_markup("Authentication error, please try again.")
-            @frame.override_background_color(:normal, Gdk::RGBA.new(1, 0, 0, 1)) # Color azul
-
-            puts "Authentication error, please try again."
-            @thread.kill if @thread
-            rfid
-            
-        end    
-    end
-    require "gtk3"
-require "thread"
-require_relative 'LCDController'
-require_relative 'Rfid'
-
-class MainWindow < Gtk::Window
-    def initialize(lcd_controller)
-        @lcd_controller = lcd_controller # Crear la instancia de LCDController
-        
-        @window = Gtk::Window.new("course_manager.rb")
-        @window.set_default_size(500, 200) # Configurar el tamaño de la ventana
-        
-        
-        
-        # Conectar la señal "destroy" para cerrar la aplicación cuando se cierra la ventana
-        @window.signal_connect("destroy") do
-            Gtk.main_quit
-            @thread.kill if @thread #Detiene la ejecución del thread
-        end 
-
-        ventana_inicio # Crear el contenido de la ventana_inicio
-        
-
-    end   
-
-    def ventana_inicio
-        # Crear un marco para enmarcar el mensaje
-        @frame = Gtk::Frame.new
-        @frame.set_border_width(10)
-        @frame.override_background_color(:normal, Gdk::RGBA.new(0, 0, 1, 1)) # Color azul
-        
-        # Crear un contenedor Gtk::Box dentro del marco para organizar verticalmente
-        box = Gtk::Box.new(:vertical, 5)
-        @frame.add(box)
-
-        # Mensaje antes de la autenticación
-        @label = Gtk::Label.new("Please, login with your university card")
-        @label.override_color(:normal, Gdk::RGBA.new(1, 1, 1, 1)) # Color blanco
-        @label.set_halign(:center) # Centrar el texto horizontalmente en la etiqueta
-        box.pack_start(@label, expand: true, fill: true, padding: 10)
-
-        @lcd_controller.escribir_en_lcd(" Please, login with your university card") # Mostrar el mensaje inicial en la LCD
-        @window.add(@frame) # Agregar el marco a la ventana
-
-        @window.show_all
-        rfid
-    end   
-
-    def rfid
-        @rfid = Rfid.new
-        iniciar_lectura_rfid # Iniciar lectura RFID
-    end
-
-    def iniciar_lectura_rfid
-        # Crea un thread para leer el uid
-        thread = Thread.new do
-            @uid = @rfid.read_uid
-            GLib::Idle.add do
-                autenticacion(@uid)
-                false # Para detener la repetición de la llamada a la función
-            end
-        end
-    end
-
-    def autenticacion(uid)
-        uri =URI("http://192.168.150.128:9000/students?student_id=#{uid}")
+        uri =URI("http://127.0.0.1:9000/students?student_id=#{uid}")
 	response = NET::HTTP.get(uri)
         if response.code == '200'
             datos=JSON.parse(response.body)
