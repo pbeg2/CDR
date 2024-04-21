39838a57a5208f172596a31660b051caf2e4f6c8
pbeg2 <166719334+pbeg2@users.noreply.github.com>
2024-04-21 18:11:43 +0200

    Update datosjson.rb

--- datosjson.rb
+++ datosjson.rb
@@ -4,23 +4,24 @@ require 'net/http'
 
 def mostrar_datos_json(url, titulo, headers)
   # Obtener los datos JSON desde la URL
-  uri = URI(url) 
-  #peticion 
+  uri = URI(url)
   json_content = Net::HTTP.get(uri)
-  if  json_content.code == '200'
+
+  if json_content.code == '200'
     # Parsear el JSON
-      datos = JSON.parse(json_content)
+    datos = JSON.parse(json_content)
   else
-      puts "Error: #{response.code}"   
+    puts "Error: #{json_content.code}"
+    return
   end
-  # Obtener la lista correspondiente segÃºn el tÃ­tulo
+
+  # Obtener la lista correspondiente según el título
   lista = datos[titulo]
 
   # Crear la ventana para mostrar los datos
   ventana = Gtk::Window.new
   ventana.set_title(titulo)
   ventana.set_default_size(400, 300)
-  
 
   # Crear un contenedor de tipo Grid
   grid = Gtk::Grid.new
@@ -35,16 +36,15 @@ def mostrar_datos_json(url, titulo, headers)
     grid.attach(header_label, index, 0, 1, 1)
   end
 
-  # Acceder a los datos y mostrar informaciÃ³n sobre cada uno
+  # Acceder a los datos y mostrar información sobre cada uno
   lista.each_with_index do |item, row_index|
     item.each_with_index do |(_, value), column_index|
       tarea_label = Gtk::Label.new(value.to_s)
       grid.attach(tarea_label, column_index, row_index + 1, 1, 1)
       if row_index % 2 == 0
-              tarea_label.override_background_color(:normal, Gdk::RGBA.new(0.7, 0.7, 1.0, 1.0)) # Azul claro
+        tarea_label.override_background_color(:normal, Gdk::RGBA.new(0.7, 0.7, 1.0, 1.0)) # Azul claro
       else
-              tarea_label.override_background_color(:normal, Gdk::RGBA.new(0.5, 0.5, 1.0, 1.0)) # Azul más oscuro
-      
+        tarea_label.override_background_color(:normal, Gdk::RGBA.new(0.5, 0.5, 1.0, 1.0)) # Azul más oscuro
       end
     end
   end
@@ -53,14 +53,13 @@ def mostrar_datos_json(url, titulo, headers)
   ventana.show_all
 end
 
-def ejecutar_todo
 # Crear la ventana principal
 window = Gtk::Window.new
 window.set_title("Consulta de archivos JSON")
 window.set_default_size(400, 100)
-window.signal_connect("destroy"){Gtk.main_quit}
+window.signal_connect("destroy") { Gtk.main_quit }
 
-# Crear el campo de entrada para el query
+# Crear el campo de entrada para la query
 query_entry = Gtk::Entry.new
 query_entry.set_placeholder_text("Ingrese query (timetable, tasks, marks)")
 
@@ -70,24 +69,25 @@ query_entry.signal_connect("activate") do
 
   case query
   when "timetables"
-    mostrar_datos_json('http://192.168.150.128:9000/horario', 'horario', ['Di­a', 'Hora', 'Materia', 'Aula'])
+    mostrar_datos_json('http://172.20.10.10:9000/horario', 'horario', ['Día', 'Hora', 'Materia', 'Aula'])
   when "tasks"
-    mostrar_datos_json('http://192.168.150.128:9000/tasks', 'tasks', ['Fecha', 'Materia', 'Nombre'])
+    mostrar_datos_json('http://172.20.10.10:9000/tasks', 'tasks', ['Fecha', 'Materia', 'Nombre'])
   when "marks"
-    mostrar_datos_json('http://192.168.150.128:9000/marks', 'marks', ['Asignatura', 'Nombre', 'Nota'])
+    mostrar_datos_json('http://172.20.10.10:9000/marks', 'marks', ['Asignatura', 'Nombre', 'Nota'])
   else
-    puts "Consulta no valida: #{query}"
+    puts "Consulta no válida: #{query}"
   end
 
-  # Limpiar el campo de entrada despuÃ©s de la consulta
+  # Limpiar el campo de entrada después de la consulta
   query_entry.text = ""
 end
 
 # Agregar el campo de entrada a la ventana principal
 window.add(Gtk::Box.new(:vertical, 5).add(query_entry))
+
+# Mostrar la ventana principal
 window.show_all
 
 # Iniciar el ciclo principal de eventos
 Gtk.main
 
-end
