945d3aa17a28cd870a25597f8c211ec9521de387
pbeg2 <166719334+pbeg2@users.noreply.github.com>
2024-04-21 05:12:34 +0200

    Update puzzle1_lcd.rb

--- puzzle1_lcd.rb
+++ puzzle1_lcd.rb
@@ -12,5 +12,15 @@ class LCDController
          @display.text(linea[0, 20], index)
     end
   end
+  def escribir_en_lcd_centrado(mensaje)
+    # Calcula cuántos espacios en blanco necesitas agregar al principio del mensaje
+    espacios_en_blanco = [(20 - mensaje.length) / 2, 0].max
 
+    # Construye el mensaje centrado con espacios en blanco al principio
+    mensaje_centrado = " " * espacios_en_blanco + mensaje
+
+    # Escribir el mensaje centrado en la pantalla LCD
+    escribir_en_lcd(mensaje_centrado)
+  end
 end
+
